396b09ded3fa37b744d852de195ce4a89a46c811
gohar-anjum <goharanjum49@gmail.com>
2024-11-09 13:09:09 +0500

    Simpler Version

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const profileButton = document.querySelector('.profile');
+    
+    profileButton.addEventListener('click', () => {
+        alert('Profile options go here!');
+    });
+    
+    // Add more JavaScript functionality as required
+});
